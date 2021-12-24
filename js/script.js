899a2b2dba51a78146442d8ed224c2c075cc6c20
Ananya <avagadi15@gmail.com>
2021-12-24 11:09:46 +0530

    jQuery used, style intact

--- js/script.js
+++ js/script.js
@@ -1,31 +1,13 @@
+$("#submit").click(function(){
+  a = document.getElementById('name').value
+  b = document.getElementById('phone').value
+  c = document.getElementById('email').value
+  d = document.getElementById('address').value
+  $('#name').val('');
+  $('#phone').val('');
+  $('#email').val('');
+  $('#address').val('');
 
-
-let form=document.getElementById("formSubmission");
-
-let table=document.getElementById('data');
-form.addEventListener("submit",(e)=>{
-  e.preventDefault();
-  submit();
-})
-
-
-const submit=()=>{
-  let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let address = document.getElementById("address").value;
-
-
-  let newArray = [name,email,phone,address];
-  newArray.forEach((item)=>{
-      var li = document.createElement("li");
-  var text = document.createTextNode(item);
-  li.appendChild(text);
-  table.appendChild(li);
-  })
-  form.reset(); 
-}
-
-// 1. on submit capture all values in different variables
-// 2. on submit clear all input fields
-// 3. append those captured values inside table using append
+  var newRowContent = "<tr><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+d+"</td></tr>";
+  $(newRowContent).appendTo($("#something1"));
+});
